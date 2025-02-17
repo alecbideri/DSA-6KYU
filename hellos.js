8400666bb0e48f362cf618f897f694846daab7cb
Bideri Alec <101627722+alecbideri@users.noreply.github.com>
2025-02-17 08:57:41 +0200

    New array katas

--- hellos.js
+++ hellos.js
@@ -1,14 +1,14 @@
 function bouncing(h, bounce, window) {
   if (h <= 0 || bounce >= 1 || bounce <= 0 || window >= h) return -1;
 
-  let times = 1; // First fall is always visible
+  let times = 1; 
 
   while (h * bounce > window) { 
-    h *= bounce;  // Bounce up
-    times += 2;   // Seen going up + seen falling down
+    h *= bounce;  
+    times += 2;   
   }
 
   return times;
 }
 
-console.log(bouncing(3.0, 0.66, 1.5)); // Output: 3
+console.log(bouncing(3.0, 0.66, 1.5)); 
