91d5d0808444ba1f995bf81eee97e39a0a55647a
Bideri Alec <101627722+alecbideri@users.noreply.github.com>
2025-02-21 23:08:07 +0200

    new katas

--- hellos.js
+++ hellos.js
@@ -1,15 +1,19 @@
 function longestConsec(strings, k) {
-  if (k <= 0 || k > strings.length) return "";
+  if (k <= 0 || k > strarr.length) return ""; 
 
   let maxString = "";
   let maxLength = 0;
 
-  for (let i = 0; i <= strings.length - k; i++) {
-    let currentString = strings.slice(i, i + k).join(""); 
+ 
+  for (let i = 0; i <= strarr.length - k; i++) {
+    let currentString = strarr.slice(i, i + k).join(""); 
+    if (currentString.length > maxLength) {
       maxLength = currentString.length;
       maxString = currentString;
     }
-
-    return maxString;
   }
+
+  return maxString;
+
+}
 
